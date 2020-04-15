1777cee6195d13d447aa59df4dd7f3231f5d0762
sambuddinc <budd.samuel@gmail.com>
2020-04-15 12:10:05 +0100

    initial commit

---
+++ fetal-ultrasound-metrics/app.py
@@ -0,0 +1,5 @@
+import streamlit
+
+st.title('Fetal Ultrasound Metrics Demonstration')
+st.header('Welcome to our automated fetal screening solution')
+st.subheader('Browse examples or upload your own')
